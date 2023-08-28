3371bd6372d8f227b04db9b3acc4c492457042ea
Dário Zongo Garcia Nzita <92880647+DARIONZITA@users.noreply.github.com>
2023-08-28 21:58:15 +0100

    Update About.tsx

--- src/app/(sections)/About.tsx
+++ src/app/(sections)/About.tsx
@@ -13,7 +13,7 @@ export function About(){
               
                 />
             </aside>
-            <aside className="animate-fade-left animate-once animate-ease-in md:w-2/3 border-t-8 md:border-l-8 sm:pl-4 border-principal-900 border-solid text-base md:text-2xl m-2 font-normal text-justify indent-3 md:border-t-0  w-[80%]">
+            <aside className="animate-fade-left animate-once animate-ease-in md:w-2/3 border-t-8 md:border-l-8 sm:pl-4 border-principal-900 border-solid text-base md:text-2xl m-2 font-normal text-justify indent-3 md:border-t-0  w-[95%]">
             <h1 className="text-5xl m-2 font-textTema indent-0">About Me</h1>
 
             <p >My name is Dário Nzita and I&apos;m a full stack 
